f9e76775701fddb21ac9491f48c44f932f926bb7
coding-learn11 <82395877+coding-learn11@users.noreply.github.com>
2023-08-04 15:49:17 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
-    video.position(110, 60)
+    video.position(100, 90)
 
-    canvas = createCanvas(500, 390);
-    canvas.position(685, 120);
+    canvas = createCanvas(550, 440);
+    canvas.position(750, 120);
 
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
@@ -12,6 +16,10 @@ function setup() {
 
 function draw() {
     background('#cbd5f0');
+    text('Aish', 100, 100);
+    textSize(difference);
+    fill('#000000');
+    
 }
 
 function modelLoaded() {
@@ -21,5 +29,9 @@ function modelLoaded() {
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX = " + leftWristX + " rightWristX = " + rightWristX + " difference = " + difference);
     }
 }
